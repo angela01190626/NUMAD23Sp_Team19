b4aba7e988f01f73d191c2ae30817f499f112aa1
wangfan-columbia <70447828+wangfan-columbia@users.noreply.github.com>
2023-03-28 17:50:55 -0400

    homepage-almost finished

--- CS5520GroupProject/app/src/main/java/edu/northeastern/cs5520groupproject/GroupProject.java
+++ CS5520GroupProject/app/src/main/java/edu/northeastern/cs5520groupproject/GroupProject.java
@@ -14,6 +14,7 @@ import android.widget.SearchView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.northeastern.cs5520groupproject.PE_Circle.UI.homepage_recycleView.ImageAdapter;
+import edu.northeastern.cs5520groupproject.PE_Circle.UI.homepage_recycleView.SpaceItemDecoration;
 
 
 public class GroupProject extends AppCompatActivity {
@@ -28,13 +29,15 @@ public class GroupProject extends AppCompatActivity {
 
         //searchView = findViewById(R.id.home_search_view);
         //bottomNavigationView = findViewById(R.id.nav_view);
-
         RecyclerView recyclerView = findViewById(R.id.image_recycler_view);
         StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
+
         recyclerView.setAdapter(new ImageAdapter(new int[]{R.drawable.sample1, R.drawable.sample2,
                 R.drawable.sample3,R.drawable.sample4,R.drawable.sample5,R.drawable.sample6,R.drawable.sample7}));
 
+        int space = getResources().getDimensionPixelSize(R.dimen.item_space); // 从资源文件中获取间距大小
+        recyclerView.addItemDecoration(new SpaceItemDecoration(space));
 
 
 
---
+++ CS5520GroupProject/app/src/main/java/edu/northeastern/cs5520groupproject/PE_Circle/UI/homepage_recycleView/SpaceItemDecoration.java
@@ -0,0 +1,27 @@
+package edu.northeastern.cs5520groupproject.PE_Circle.UI.homepage_recycleView;
+
+import android.graphics.Rect;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SpaceItemDecoration extends RecyclerView.ItemDecoration {
+    private final int space;
+
+    public SpaceItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        outRect.left = space;
+        outRect.right = space;
+        outRect.bottom = space/2;
+
+        if (parent.getChildLayoutPosition(view) == 0) {
+            outRect.top = space;
+        } else {
+            outRect.top = space / 2; // 减小上下间距
+        }
+    }
+}
