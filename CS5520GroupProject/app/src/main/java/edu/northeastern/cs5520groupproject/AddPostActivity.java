c23a285d8e1f54e805d70d2b8e902dd3cd148ac2
Renzhao Li <li.re@northeastern.edu>
2023-04-17 07:18:50 -0400

    making posts and reading feed

---
+++ CS5520GroupProject/app/src/main/java/edu/northeastern/cs5520groupproject/AddPostActivity.java
@@ -0,0 +1,155 @@
+package edu.northeastern.cs5520groupproject;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddPostActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_IMAGE_PICK = 1000;
+
+    private ImageView imageView;
+    private EditText editTextDescription;
+    private Uri imageUri;
+
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+
+        imageView = findViewById(R.id.imageView);
+        editTextDescription = findViewById(R.id.editTextDescription);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("posts");
+        storageReference = FirebaseStorage.getInstance().getReference("post_images");
+    }
+
+    public void selectImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_CODE_IMAGE_PICK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_IMAGE_PICK && resultCode == RESULT_OK && data != null) {
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                File tempFile = File.createTempFile("temp_image", null, getCacheDir());
+                FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                fileOutputStream.flush();
+                fileOutputStream.close();
+                inputStream.close();
+
+                imageUri = Uri.fromFile(tempFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Glide.with(this).load(imageUri).into(imageView);
+        }
+    }
+
+    public void uploadImageAndDescription(View view) {
+        if (imageUri == null || editTextDescription.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please select an image and enter a description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Bitmap bitmap;
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to decode image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageData = baos.toByteArray();
+
+        final StorageReference fileRef = storageReference.child(System.currentTimeMillis() + ".jpeg");
+        UploadTask uploadTask = fileRef.putBytes(imageData);
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                return fileRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    String postId = databaseReference.push().getKey();
+                    Map<String, Object> post = new HashMap<>();
+                    post.put("postId", postId);
+                    post.put("imageUrl", downloadUri.toString());
+                    post.put("description", editTextDescription.getText().toString().trim());
+
+                    databaseReference.child(postId).setValue(post);
+                    Toast.makeText(AddPostActivity.this, "Post uploaded successfully", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(AddPostActivity.this, "Upload failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private String getFileExtension(Uri uri) {
+        String extension = "";
+        if (uri.getScheme().equals("content")) {
+            extension = getContentResolver().getType(uri).split("/")[1];
+        } else {
+            extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+        }
+        return extension;
+    }
+
+}
