01486267bd972fb627a0dd63af8110125d23a9ea
wangfan-columbia <70447828+wangfan-columbia@users.noreply.github.com>
2023-04-21 11:54:02 -0400

    add more coach and upload coach info to firebase

--- CS5520GroupProject/app/src/main/java/edu/northeastern/cs5520groupproject/PE_Circle/UI/homepage_recycleView/imageItem.java
+++ CS5520GroupProject/app/src/main/java/edu/northeastern/cs5520groupproject/PE_Circle/UI/homepage_recycleView/imageItem.java
@@ -5,4 +5,26 @@ public class imageItem {
     private int image;
     private String username;
 
+    public imageItem(){ }
+
+    public imageItem(int image, String username) {
+        this.image = image;
+        this.username = username;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
